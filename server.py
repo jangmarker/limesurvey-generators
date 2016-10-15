bc7262757fc09caa2b6f38a12a22506ea6f5cbd0
Jan Marker <jan@jangmarker.de>
2016-10-15 02:34:46 +0200

    Simple server

---
+++ server.py
@@ -0,0 +1,24 @@
+from ast import literal_eval
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from logging import fatal
+from urllib.parse import parse_qs, urlparse
+
+from OrderQuestionGenerator import expression
+
+
+class GeneratorServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        url = urlparse(self.path)
+        if url.path == '/orderquestion':
+            query = parse_qs(url.query)
+            question_name = query['name']
+            reference = literal_eval('[' + query['answer'][0] + ']')
+
+            output = expression(question_name, reference)
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(output.encode())
+
+httpd = HTTPServer(("", 8080), GeneratorServer)
+httpd.serve_forever()
