dd15c31e9b9df47c9996c2bbe15335b376e98fbb
Jan Marker <jan@jangmarker.de>
2016-10-15 17:20:45 +0200

    Expose multiple choice generator

--- server.py
+++ server.py
@@ -3,22 +3,35 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from logging import fatal
 from urllib.parse import parse_qs, urlparse
 
-from OrderQuestionGenerator import expression
+import MultipleChoiceQuestionGenerator
+import OrderQuestionGenerator
+
+
+def extract_array(query, key):
+    return literal_eval('[' + query[key][0] + ']')
 
 
 class GeneratorServer(BaseHTTPRequestHandler):
     def do_GET(self):
         url = urlparse(self.path)
+        query = parse_qs(url.query)
+        question_name = query['name'][0]
         if url.path == '/orderquestion':
-            query = parse_qs(url.query)
-            question_name = query['name'][0]
-            reference = literal_eval('[' + query['answer'][0] + ']')
+            reference = extract_array(query, 'answer')
+            output = OrderQuestionGenerator.expression(question_name, reference)
+            status = 200
+        elif url.path == '/multiplechoicequestion':
+            correct = extract_array(query, 'correct')
+            wrong = extract_array(query, 'wrong')
+            output = MultipleChoiceQuestionGenerator.generate(question_name, correct, wrong)
+            status = 200
+        else:
+            output = "route does not exist"
+            status = 404
 
-            output = expression(question_name, reference)
-
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(output.encode())
+        self.send_response(status)
+        self.end_headers()
+        self.wfile.write(output.encode())
 
 if __name__ == '__main__':
     httpd = HTTPServer(("", 42387), GeneratorServer)
